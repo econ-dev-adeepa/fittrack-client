7f31d8a3a5d884c5e6392ef8584550df4a4d4a4f
Adeepa Gunathilake <adeepa.g@econsulate.net>
2026-03-17 16:35:22 +0530

    feat: implement core auth logic in login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,59 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { exchangeCodeAsync, makeRedirectUri, useAuthRequest, useAutoDiscovery } from "expo-auth-session"
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+  const discovery = useAutoDiscovery(process.env.EXPO_PUBLIC_KEYCLOAK_URL);
+
+  const redirectUri = makeRedirectUri({
+    scheme: "fittrack",
+  })
+
+  console.log('Redirect URI:', redirectUri);
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "fittrack-client",
+      redirectUri,
+      scopes: ["openid", "profile", "email"],
+    },
+    discovery
+  );
+
+  const exchangeCodeForToken = async (code: string) => {
+    const tokenResult = await exchangeCodeAsync(
+      {
+        clientId: "fittrack-client",
+        code: code,
+        redirectUri: redirectUri,
+        extraParams: {
+          code_verifier: request!.codeVerifier!, 
+        },
+      },
+      discovery!
+    );
+
+    console.log('Access Token (JWT):', tokenResult.accessToken);
+    console.log('Refresh Token:', tokenResult.refreshToken);
+    console.log('ID Token:', tokenResult.idToken);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      try {
+        exchangeCodeForToken(code);
+      } catch (error) {
+        console.error('Token Exchange Error:', error);
+      }
+    } else if (response?.type === 'error') {
+      console.error('Auth Error:', response.error);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
       <View style={styles.heroWrapper}>
@@ -14,7 +67,7 @@ export default function LoginScreen() {
       <View style={styles.actions}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => {}}
+          onPress={() => promptAsync()}
         >
           <Text style={styles.buttonText}>Sign In</Text>
         </TouchableOpacity>
